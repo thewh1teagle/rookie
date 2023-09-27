3e6ff51c72feba4397a4c66f6b26fe6a6218d04a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-27 16:07:06 +0300

    feat: get fields from query

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::{env, path::{self, PathBuf}, fs, ptr};
-
+use std::{env, path::{self, PathBuf}, fs, ptr, fmt};
+use time::OffsetDateTime;
+use cookie::Expiration;
 use serde_json;
 use base64::{Engine as _, engine::general_purpose};
 
@@ -27,16 +28,35 @@ use aes_gcm::{
 };
 
 
-// struct Cookie {
-//     host: String,
-//     path:     String,
-// 	secure:   bool,
-// 	expires:  String,
-// 	name:     String,
-// 	value:    String,
-// 	http_only: bool,
-// 	same_site: i32
-// }
+struct Cookie {
+    host: String,
+    path:     String,
+	secure:   bool,
+	expires:  i64,
+	name:     String,
+	value:    String,
+	http_only: bool,
+	same_site: i64
+}
+
+
+impl fmt::Display for Cookie {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Cookie:\n\
+             - Host: {}\n\
+             - Path: {}\n\
+             - Secure: {}\n\
+             - Expires: {}\n\
+             - Name: {}\n\
+             - Value: {}\n\
+             - Http Only: {}\n\
+             - Same Site: {}",
+            self.host, self.path, self.secure, self.expires, self.name, self.value, self.http_only, self.same_site
+        )
+    }
+}
 
 fn find_chrome_paths() -> (PathBuf, PathBuf) {
     let appdata_path = env::var("APPDATA").unwrap();
@@ -116,8 +136,6 @@ fn decrypt_encrypted_value(value: &[u8], key: &[u8]) -> String {
 }
 
 fn query_cookies(v10_key: Vec<u8>, db_path: PathBuf) {
-    // let mut db_path = db_path.canonicalize().unwrap().as_os_str().to_str().unwrap().to_string();
-    // println!("{}", db_path);
     let connection = sqlite::open(db_path).unwrap();
     let query = "
         SELECT host_key, path, is_secure, expires_utc, name, value, encrypted_value, is_httponly, samesite
@@ -128,10 +146,30 @@ fn query_cookies(v10_key: Vec<u8>, db_path: PathBuf) {
     .unwrap()
     .into_iter()
     .map(|row| row.unwrap()) {
+        let host_key = row.read::<&str, _>("host_key");
+        let path = row.read::<&str, _>("path");
+        let is_secure = row.read::<i64, _>("is_secure") != 0;
+        let expires_nt_time_epoch = row.read::<i64, _>("expires_utc");
+        let name = row.read::<&str, _>("name");
+        
+
         let encrypted_value = row.read::<&[u8], _>("encrypted_value");
-        let decrypted = decrypt_encrypted_value(encrypted_value, &v10_key);
-        let host = row.read::<&str, _>("host_key");
-        println!("host: {} value: {}", host, decrypted);
+        let decrypted_value = decrypt_encrypted_value(encrypted_value, &v10_key);
+        let http_only = row.read::<i64, _>("is_httponly") != 0;
+        
+        let same_site = row.read::<i64, _>("samesite");
+        let cookie = Cookie {
+            host: host_key.to_string(),
+            path: path.to_string(),
+            secure: is_secure,
+            expires: expires_nt_time_epoch,
+            name: name.to_string(),
+            value: decrypted_value,
+            http_only,
+            same_site
+        };
+        println!("{}", cookie);
+
     }
 }
 

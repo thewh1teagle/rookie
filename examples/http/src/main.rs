342cac103744bcfa265b7c05dce170ee44262b8a
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-28 19:01:48 +0300

    feat: add github http example

---
+++ examples/http/src/main.rs
@@ -0,0 +1,34 @@
+use reqwest::blocking::Client;
+use rookie::chrome;
+use rookie::enums::CookieToString;
+use regex::Regex;
+
+
+fn extract_username(html: &str) -> &str {
+    let re = Regex::new(r#"dashboard\/ajax_context_list\?current_context=(.+)""#).unwrap();
+    if let Some(capture) = re.captures(html) {
+        if let Some(content) = capture.get(1) {
+            return content.as_str();
+        }
+    }
+    ""
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a custom cookie store
+    let client = Client::new();
+    let cookies = chrome(Some(vec!["github.com"]))?;
+    let response = client.get("https://github.com/")
+    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+    .header("Cookie", cookies.to_string()) // <- try to comment
+    .send()?;
+
+    
+    let content = response.text()?;
+    let username = extract_username(content.as_str());
+    match username {
+        "" => println!("Not logged in to Github"),
+        _ => println!("Logged in to Github as {}", username)
+    };
+    Ok(())
+}

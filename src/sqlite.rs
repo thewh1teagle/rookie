6932dcb5323b20e14b64ea9ef3efe62b01f2c3e6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-29 21:27:05 +0300

    remove print

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -5,6 +5,5 @@ use std::path::PathBuf;
 pub fn connect(path: PathBuf) -> Connection {
     let flags = OpenFlags::SQLITE_OPEN_READ_ONLY | OpenFlags::SQLITE_OPEN_URI;
     let conn_str = format!("{}?mode=ro&immutable=1", Url::from_file_path(path).unwrap());
-    println!("{}", conn_str);
     rusqlite::Connection::open_with_flags(conn_str, flags).unwrap()
 }

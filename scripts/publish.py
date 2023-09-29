4caadadf205a97da25d6304665904d29693d9e4e
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-29 23:51:00 +0300

    add script and increment version

---
+++ scripts/publish.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import re
+
+
+def increment_ver(version):
+    version = version.split('.')
+    version[2] = str(int(version[2]) + 1)
+    return '.'.join(version)
+
+cur = Path(__file__).parent
+cargo_paths = [cur / '../rookie-rs', cur / '../bindings/python']
+
+
+for path in cargo_paths:
+    toml = path / 'cargo.toml'
+    content = toml.open('r').read()
+    pattern = 'version = "(.+)"[\S\s]edition = ".+"'
+    ver = re.search(pattern, content)
+    if ver:
+        new_ver = increment_ver(ver.group(1))
+        new_content = re.sub(pattern, f'version = "{new_ver}"\nedition = "2021"', content)
+        with toml.open('w') as f:
+            f.write(new_content)

0afdb73d6a1cf212b60beba39ba36a916d8dcb7e
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-09-30 15:40:46 +0300

    fix: add macos implement for chromium based dec

--- rookie-rs/src/chromium.rs
+++ rookie-rs/src/chromium.rs
@@ -42,7 +42,7 @@ fn get_v10_key() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 
 
 #[cfg(target_os = "macos")]
-fn get_v10_key() -> Result<Vec<u8>, bcrypt_pbkdf::Error> {
+fn get_v10_key() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     use pbkdf2::pbkdf2_hmac;
     use sha1::Sha1;
 
@@ -75,6 +75,34 @@ fn decrypt_encrypted_value(value: String, encrypted_value: &[u8], key: &[u8]) ->
 }
 
 #[cfg(target_os = "linux")]
+fn decrypt_encrypted_value(value: String, encrypted_value: &[u8], key: &[u8]) -> String {
+    let key_type = &encrypted_value[..3];
+    if !value.is_empty() || !(key_type == b"v11" || key_type == b"v10") { // unknown key_type or value isn't encrypted
+        return value;
+    }
+    use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, KeyIvInit};
+    
+    type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
+
+
+    // Create an AES-128 cipher with the provided key.
+
+
+    let encrypted_value = & mut encrypted_value.to_owned()[3..];
+    let iv: [u8; 16] = [b' '; 16];
+
+    println!("{:?}", key);
+    let mut  key_array: [u8;16] = [0;16];
+    key_array.copy_from_slice(&key[..16]);
+    let cipher = Aes128CbcDec::new(&key_array.into(), &iv.into());
+
+    let plaintext = cipher.decrypt_padded_mut::<Pkcs7>(encrypted_value).unwrap();
+
+
+    String::from_utf8(plaintext.to_vec()).unwrap()
+}
+
+#[cfg(target_os = "macos")]
 fn decrypt_encrypted_value(value: String, encrypted_value: &[u8], key: &[u8]) -> String {
     let key_type = &encrypted_value[..3];
     if !value.is_empty() || !(key_type == b"v11" || key_type == b"v10") { // unknown key_type or value isn't encrypted

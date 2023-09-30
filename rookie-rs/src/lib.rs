f0f6800ab72488ff0e2058a1e8895a8636e6b099
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-10-01 01:46:12 +0300

    feat: add documentation to lib.rs

--- rookie-rs/src/lib.rs
+++ rookie-rs/src/lib.rs
@@ -15,7 +15,20 @@ pub use mozilla::firefox_based;
 pub use enums::*;
 
 
-
+/// Returns cookies from all browsers
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::load(Some(domains));
+/// ```
 pub fn load(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let mut cookies = Vec::new();
 
@@ -36,38 +49,138 @@ pub fn load(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     Ok(cookies)
 }
 
+/// Returns cookies from firefox
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::firefox(Some(domains));
+/// ```
 pub fn firefox(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let db_path = paths::find_mozilla_based_paths(&config::FIREFOX_CONFIG)?;
     firefox_based(db_path, domains)
 }
 
+
+/// Returns cookies from chrome
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::chrome(Some(domains));
+/// ```
 pub fn chrome(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::CHROME_CONFIG)?;
     chromium_based(key, db_path, domains)
 }
 
+/// Returns cookies from chromium
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::chromium(Some(domains));
+/// ```
 pub fn chromium(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::CHROMIUM_CONFIG)?;
     chromium_based(key, db_path, domains)
 }
 
 
+/// Returns cookies from brave
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::brave(Some(domains));
+/// ```
 pub fn brave(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::BRAVE_CONFIG)?;
     chromium_based(key, db_path, domains)
 }
 
+
+/// Returns cookies from edge
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::edge(Some(domains));
+/// ```
 pub fn edge(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::EDGE_CONFIG)?;
     chromium_based(key, db_path, domains)
 }
 
-
+/// Returns cookies from vivaldi
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::vivaldi(Some(domains));
+/// ```
 pub fn vivaldi(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::VIVALDI_CONFIG)?;
     chromium_based(key, db_path, domains)
 }
 
+
+/// Returns cookies from opera
+///
+/// # Arguments
+///
+/// * `domains` - A optional list that for getting specific domains only
+///
+/// # Examples
+///
+/// ```
+///
+/// use rookie;
+/// let domains = vec!["google.com"];
+/// let cookies = rookie::opera(Some(domains));
+/// ```
 pub fn opera(domains: Option<Vec<&str>>) -> Result<Vec<Cookie>, Box<dyn Error>> {
     let (key, db_path) = paths::find_chrome_based_paths(&config::OPERA_CONFIG)?;
     chromium_based(key, db_path, domains)

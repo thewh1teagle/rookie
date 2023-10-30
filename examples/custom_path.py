a24d6c35bbfd2f36fb917496274725aaa260d3e9
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-10-30 23:22:15 +0200

    add custom_path example

---
+++ examples/custom_path.py
@@ -0,0 +1,10 @@
+import rookiepy
+from pathlib import Path
+
+
+# Using pathlib and cross platform paths is always recommended!
+db_path = Path("C:/Users/User/AppData/Local/BraveSoftware/Brave-Browser/User Data/default/network/Cookies").resolve().absolute()
+key_path = Path("C:/Users/User/AppData/Local/BraveSoftware/Brave-Browser/User Data/Local State").resolve().absolute() # optional
+db_path, key_path = str(db_path), str(key_path)
+cookies = rookiepy.any_browser(db_path=db_path, key_path=key_path, domains=None)
+print(cookies)

24db91f0052196cb25a6740da8ce5c7ad4614e34
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-10-30 21:46:38 +0200

    fix(cli): cross platform chromium

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
-use rookie::{chromium_based, firefox_based, config, Cookie};
+use rookie::{chromium_based, firefox_based, Cookie};
 use serde_json;
+use std::path::PathBuf;
 
 
 #[derive(Parser, Debug)]
@@ -11,6 +12,9 @@ struct Args {
     path: String,
 
     #[arg(short, long)]
+    key_path: Option<String>,
+
+    #[arg(short, long)]
     domains: Vec<String>
 }
 
@@ -19,26 +23,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let domains: Vec<&str> = args.domains.iter().map(|d| d.as_str()).collect();
     let mut cookies: Vec<Cookie> = vec![];
 
-    let chrome_configs = &[
-        &config::CHROME_CONFIG,
-        &config::BRAVE_CONFIG,
-        &config::CHROMIUM_CONFIG,
-        &config::EDGE_CONFIG,
-        &config::OPERA_CONFIG,
-        &config::OPERA_GX_CONFIG,
-        &config::VIVALDI_CONFIG,
-    ];
-
-    // chromium
-    for browser_config in chrome_configs {
-        let res_cookies = chromium_based(&browser_config, args.path.to_owned().into(), Some(domains.to_owned()))?;
-        cookies.extend(res_cookies);
-        if let Ok(str) = serde_json::to_string_pretty(&cookies) {
-            println!("{}", str);
-            return Ok(());
+    cfg_if::cfg_if! {
+        if #[cfg(unix)] {
+            use rookie::config;
+            let chrome_configs = &[
+                &config::CHROME_CONFIG,
+                &config::BRAVE_CONFIG,
+                &config::CHROMIUM_CONFIG,
+                &config::EDGE_CONFIG,
+                &config::OPERA_CONFIG,
+                &config::OPERA_GX_CONFIG,
+                &config::VIVALDI_CONFIG,
+            ];
+            for browser_config in chrome_configs {
+                let res_cookies = chromium_based(&browser_config, args.path.clone().into(), Some(domains.to_owned()))?;
+                cookies.extend(res_cookies);
+            }
+        } else {
+            if let Some(key_path) = args.key_path {
+                let res_cookies = chromium_based(PathBuf::from(key_path), args.path.to_owned().into(), Some(domains.to_owned()))?;
+                cookies.extend(res_cookies);
+            }
         }
     }
-
+    
     // mozilla
     let res_cookies = firefox_based(args.path.to_owned().into(), Some(domains.to_owned()))?;
     cookies.extend(res_cookies);
@@ -46,6 +54,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{}", str);
         return Ok(());
     }
-    
     Ok(())
 }

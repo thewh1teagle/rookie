7fe3cd23c200a862acccb86ebf157b2f789b2019
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-11-04 00:53:10 +0200

    fix: convert safari date to unix

--- rookie-rs/src/common/date.rs
+++ rookie-rs/src/common/date.rs
@@ -2,7 +2,7 @@ pub fn chromium_timestamp(timestamp: u64) -> Option<u64> {
     if timestamp <= 0 {
         return None;
     }
-    let mut timestamp = timestamp - 11644473600000000;
+    let mut timestamp = timestamp - 11_644_473_600_000_000;
     timestamp /= 1000000; // milliseconds to seconds
     unix_timestamp(timestamp)
 }
@@ -16,19 +16,21 @@ pub fn internet_explorer_timestamp(timestamp: u64) -> Option<u64> {
     if timestamp <= 0 {
         return None;
     }
-    let mut timestamp = timestamp - 116444736000000000;
-    timestamp /= 10000000;
+    let mut timestamp = timestamp - 116_444_736_000_000_000;
+    timestamp /= 10_000_000;
     unix_timestamp(timestamp)
 }
 
 
-#[cfg(target_os = "macos")]
+
 pub fn safari_timestamp(timestamp: u64) -> Option<u64> {
     if timestamp <= 0 {
         return None;
     }
-    let timestamp = timestamp + 978307200f64 as u64;
-    unix_timestamp(timestamp)
+    let unix_timestamp = timestamp + 978_307_200;
+    // nanoseconds to seconds
+    let unix_timestamp = unix_timestamp / 1_000_000_000;
+    Some(unix_timestamp)
 }
 
 fn unix_timestamp(timestamp: u64) -> Option<u64> {

d3c2b9609b5c0ddc0a14ac3f5d206bf38ff332e0
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-10-26 02:44:18 +0300

    try to include binaries in crate

--- rookie-rs/src/lib.rs
+++ rookie-rs/src/lib.rs
@@ -7,11 +7,11 @@ pub use common::enums;
 
 #[cfg(feature = "appbound")]
 #[allow(unused)]
-static PAEXEC_BYTES: &[u8] = include_bytes!("paexec"); // wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\paexec
+static PAEXEC_BYTES: &[u8] = include_bytes!("paexec.exe.rs"); // wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\paexec.exe.rs
 
 #[cfg(feature = "appbound")]
 #[allow(unused)]
-static CRYPT_UNPROTECT_BYTES: &[u8] = include_bytes!("unprotect"); // cargo build --release -p cryptunprotect OR wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\unprotect
+static CRYPT_UNPROTECT_BYTES: &[u8] = include_bytes!("unprotect.exe.rs"); // cargo build --release -p cryptunprotect OR wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\unprotect.exe.rs
 
 // Browser
 #[cfg(target_os = "windows")]

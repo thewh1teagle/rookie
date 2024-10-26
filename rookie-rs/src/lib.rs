de3bb5bd93110e6dbe3b71d17050c4cb015b75ac
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-10-26 03:20:50 +0300

    use .bin for binaries

--- rookie-rs/src/lib.rs
+++ rookie-rs/src/lib.rs
@@ -7,11 +7,11 @@ pub use common::enums;
 
 #[cfg(feature = "appbound")]
 #[allow(unused)]
-static PAEXEC_BYTES: &[u8] = include_bytes!("paexec.bin"); // wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\paexec.bin
+static PAEXEC_BYTES: &[u8] = include_bytes!("../paexec.bin"); // wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\paexec.bin
 
 #[cfg(feature = "appbound")]
 #[allow(unused)]
-static CRYPT_UNPROTECT_BYTES: &[u8] = include_bytes!("unprotect.bin"); // cargo build --release -p cryptunprotect OR wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\unprotect.bin
+static CRYPT_UNPROTECT_BYTES: &[u8] = include_bytes!("../unprotect.bin"); // cargo build --release -p cryptunprotect OR wget.exe https://github.com/thewh1teagle/rookie/releases/download/appbound-binaries/paexec.exe -O rookie-rs\src\unprotect.bin
 
 // Browser
 #[cfg(target_os = "windows")]
